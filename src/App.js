c85cb6d3985bbde908300f3fe1bcc6e891b0b356
chrsnh <chao@gbox.adnu.edu.ph>
2024-06-24 16:01:27 +0800

    workflow added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Alruch <code>src/App.js</code> Gala Well
         </p>
         <a
           className="App-link"
